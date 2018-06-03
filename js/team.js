74e3d85fc71773b17d05a18ca332e9701de108b6
memecast <=>
2018-06-03 05:30:30 -0500

    Creando referencia a Members de firebase en proyectos

--- js/team.js
+++ js/team.js
@@ -9,3 +9,18 @@ $("#question-form").on("submit",postQuestion);
 $( "document" ).ready(()=>{
     M.textareaAutoResize($('#team-questions'));
 });
+
+  // Initialize Firebase
+var config = {
+apiKey: "AIzaSyAi2rmSnCZoSehOuJNenOMqip-rRFYBXFs",
+authDomain: "poppower-8c0ef.firebaseapp.com",
+databaseURL: "https://poppower-8c0ef.firebaseio.com",
+projectId: "poppower-8c0ef",
+storageBucket: "poppower-8c0ef.appspot.com",
+messagingSenderId: "901998159862"
+};
+firebase.initializeApp(config);
+const dbRefMembers = firebase.database().ref().child("Members");
+dbRefMembers.on('child_added', (snap) => {
+    console.log(snap.val());
+});
